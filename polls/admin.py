c193b95c7efc70ffb9d388641b6a830850c83f10
zth <504759204@qq.com>
2019-09-24 14:20:03 +0800

    quesiton relatin choice

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -12,3 +12,4 @@ class QuestionAdmin(admin.ModelAdmin):
 	]
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
