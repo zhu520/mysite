0dfe810fe3c3bda470fa742a76ba81f571475272
zth <504759204@qq.com>
2019-09-24 14:30:05 +0800

    better relation with quesiton and choice

--- polls/admin.py
+++ polls/admin.py
@@ -4,12 +4,18 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+# class ChoiceInline(admin.StackedInline):	占据了大量的屏幕区域
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
 	#fields = ['pub_date', 'question_text']
 	fieldsets = [
 		(None,				 {'fields': ['question_text']}),
-		('Date information', {'fields': ['pub_date']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
 	]
+	inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
