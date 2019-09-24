3f8f605173f20a3fc1d734cc7b45b71cc15e4713
zth <504759204@qq.com>
2019-09-24 10:54:34 +0800

    restructure url views

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse, HttpResponseRedirect
+# from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 # from django.template import loader
-from django.http import Http404
+# from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
 
+	def get_queryset(self):
+		''' Return the last five published questions. '''
+		return Question.objects.order_by('-pub_date')[:5]
+'''
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
 	# template = loader.get_template('polls/index.html')
@@ -24,7 +33,12 @@ def index(request):
 	# return HttpResponse(output)
 	
 	# return HttpResponse("Hello, world. You're at the polls index.")
+'''
 
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+'''
 def detail(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
 
@@ -36,12 +50,18 @@ def detail(request, question_id):
 	return render(request, 'polls/detail.html', {'question': question})
 
 	# return HttpResponse("You're looking at question %s." % question_id)
+'''
 
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
+'''
 def results(request, question_id):
 	# response = "You're looking at the results of question %s."
 	# return HttpResponse(response % question_id)
 	question = get_object_or_404(Question, pk=question_id)
 	return render(request, 'polls/results.html', {'question': question})
+'''
 
 def vote(request, question_id):
 	# return HttpResponse("You're voting on question %s." % question_id)
