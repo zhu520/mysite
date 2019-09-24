09b55175188314c1cf1c700ce678ab0d9d98a770
zth <504759204@qq.com>
2019-09-24 13:40:07 +0800

    perfect test

--- polls/views.py
+++ polls/views.py
@@ -8,6 +8,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -17,8 +18,9 @@ class IndexView(generic.ListView):
 	context_object_name = 'latest_question_list'
 
 	def get_queryset(self):
-		''' Return the last five published questions. '''
-		return Question.objects.order_by('-pub_date')[:5]
+		''' Return the last five published questions (not including those
+		set to be published in the future). '''
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 '''
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -38,6 +40,11 @@ def index(request):
 class DetailView(generic.DetailView):
 	model = Question
 	template_name = 'polls/detail.html'
+	def get_queryset(self):
+		'''
+		Excludes any questions that aren't published yet.
+		'''
+		return Question.objects.filter(pub_date__lte=timezone.now())
 '''
 def detail(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
